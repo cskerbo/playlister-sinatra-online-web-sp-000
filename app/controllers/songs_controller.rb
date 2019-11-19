26dec3073287bba0e535f68718dc446294259f02
Chris Kerbo <chris.kerbo@outlook.com>
2019-11-19 22:05:57 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -14,16 +14,26 @@ class SongsController < ApplicationController
   end
 
   post '/songs' do
-    @song = Song.create(name: params[:song][:name])
+    @song = Song.create(:name => params[:song][:name])
 
-    @song.artist = Artist.find_by(name: params[:song][:artist])
-    if @song.artist == nil
-      @song.artist = Artist.create(name: params[:song][:artist])
+    artist_entry = params[:song][:artist]
+    if Artist.find_by(:name => artist_entry)
+      artist = Artist.find_by(:name => artist_entry)
+    else
+      artist = Artist.create(:name => artist_entry)
     end
+    @song.artist = artist
+
+    genre_selections = params[:song][:genres]
+    genre_selections.each do |genre|
+      @song.genres << Genre.find(genre)
+    end
+
     @song.save
 
     flash[:message] = "Successfully created song."
     redirect to "songs/#{@song.slug}"
+
   end
 
   get '/songs/:slug' do
