6b0fc39c1bc6445d141fc4c4f51b2a62d24d3087
Chris Kerbo <chris.kerbo@outlook.com>
2019-11-19 22:27:06 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -20,7 +20,7 @@ class SongsController < ApplicationController
   end
 
   post '/songs' do
-    @song = Song.create(params['song'])
+    @song = Song.create(:name => params[:song][:name], :artist => params[:song[:artist])
 
     artist_entry = params[:song][:artist]
     if Artist.find_by(:name => artist_entry)
