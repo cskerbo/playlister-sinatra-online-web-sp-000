45eede93c3a1d560ab85020e194bb676ffbb4991
Chris Kerbo <chris.kerbo@outlook.com>
2019-11-19 21:59:18 +0000

    Automatically backed up by Learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,9 +1,12 @@
 class GenresController < ApplicationController
-
   get '/genres' do
-    @genres = Genre.all
+      @genres = Genre.all
+      erb :"genres/index"
+    end
 
-    erb :'genres/index'
-  end
-
+    get '/genres/:slug' do
+      slug = params[:slug]
+      @genre = Genre.find_by_slug(slug)
+      erb :"genres/show"
+    end
 end
